70715b95a5b855a2da40ba5b1a728f2b0cb60465
Fajar Ihsana <67202122+Zynfinity@users.noreply.github.com>
2022-10-01 07:02:27 +0700

    Update ttstalk.js

--- commands/stalk/ttstalk.js
+++ commands/stalk/ttstalk.js
@@ -4,6 +4,7 @@ module.exports = {
   cmd: ['ttstalk', 'tiktokstalk'],
   category: 'stalk',
   query: 'Example: .tiktokstalk yourrrkayess',
+  disabled: true,
   async handler(m, {conn, text}){
     await m.reply(response.wait)
     const stalk = await scrapp.tiktokstalk(text)
